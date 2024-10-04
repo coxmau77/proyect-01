1868b01fe3ecdfb06e01e48b02bbc3123bc7291d
Mauricio Cox <coxmau77@gmail.com>
2024-10-04 18:21:29 -0300

    upload readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 EL proyecto ya esta [desplegado en Vercel](https://vercel.com/mauricios-projects-8b7b5bd9/proyect-01/DL3DEv664moYAcmDv8k3TkV7kpak) a modo de prueba.
+
+[repo](https://github.com/coxmau77/proyect-01)
--- public/temp/events.js
+++ public/temp/events.js
@@ -1,38 +1,38 @@
 let tickets = {
   river: {
     lugar: "Estadio River Plate",
-    tickets: 100,
+    tickets: 1,
     sku: "#FWB000",
     fecha: "2024-10-07",
     precio: 399999,
   },
   boca: {
     lugar: "Estadio Boca Juniors",
-    tickets: 300,
+    tickets: 3,
     sku: "#FWB001",
     fecha: "2024-10-02",
-    precio: 399999,
+    precio: 199999,
   },
   movistar: {
     lugar: "Movistar Arena",
     tickets: 1,
     sku: "#FWB002",
     fecha: "2024-10-14",
-    precio: 399999,
+    precio: 222999,
   },
   obras: {
     lugar: "Estadio Obras Sanitarias",
-    tickets: 700,
+    tickets: 7,
     sku: "#FWB003",
     fecha: "2024-10-11",
-    precio: 399999,
+    precio: 449999,
   },
   rex: {
     lugar: "Teatro Gran Rex",
     tickets: 0,
     sku: "#FWB004",
     fecha: "2024-10-01",
-    precio: 399999,
+    precio: 777999,
   },
 };
 
@@ -58,21 +58,58 @@ function mostrarEventos() {
   for (let key in tickets) {
     const evento = tickets[key];
     const li = document.createElement("li");
-    li.innerHTML = `
-      <strong>${evento.lugar}</strong><br>
-      Fecha: ${new Date(evento.fecha).toLocaleDateString("es-ES", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      })}<br>
-      Precio: $${evento.precio.toLocaleString()}<br>
-      Código SKU: ${evento.sku}<br>
-      Tickets disponibles: ${evento.tickets}<br>
-      <button id="boton-${key}" ${evento.tickets <= 0 ? "disabled" : ""}>
-        ${evento.tickets > 0 ? "Comprar tickets ahora" : "Tickets agotados"}
-      </button>
+    li.innerHTML = `<section>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd"
+                    d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                    clip-rule="evenodd" />
+            </svg>
+            <div>
+                <small>${evento.sku}</small>
+                <strong>$${evento.precio.toFixed(2)}</strong><br>
+                <strong>${evento.lugar}</strong><br>
+                <span>${new Date(evento.fecha).toLocaleDateString("es-ES", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}</span>
+            </div>
+        </section>
+            <div>
+
+                <button id="boton-${key}" ${
+      evento.tickets <= 0 ? "disabled" : ""
+    }>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+                    </svg>
+                    ${
+                      evento.tickets > 0
+                        ? "Comprar tickets ahora"
+                        : "Tickets agotados"
+                    }
+                </button>
+                <small>Tickets disponibles: ${evento.tickets}</small>
+            </div>
     `;
+    // li.innerHTML = `
+    //   <strong>${evento.lugar}</strong><br>
+    //   Fecha: ${new Date(evento.fecha).toLocaleDateString("es-ES", {
+    //     weekday: "long",
+    //     year: "numeric",
+    //     month: "long",
+    //     day: "numeric",
+    //   })}<br>
+    //   Precio: $${evento.precio.toLocaleString()}<br>
+    //   Código SKU: ${evento.sku}<br>
+    //   Tickets disponibles: ${evento.tickets}<br>
+    //   <button id="boton-${key}" ${evento.tickets <= 0 ? "disabled" : ""}>
+    //     ${evento.tickets > 0 ? "Comprar tickets ahora" : "Tickets agotados"}
+    //   </button>
+    // `;
     eventosList.appendChild(li);
 
     // Asignar evento de compra
@@ -80,6 +117,36 @@ function mostrarEventos() {
     botonCompra.addEventListener("click", () => getTickets(key));
   }
 }
+
+// Validar y capturar la información del usuario
+usuarioForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Evitar que el formulario se envíe
+
+  const nombreInput = document.getElementById("nombre");
+  const edadInput = document.getElementById("edad");
+
+  console.log(nombreInput.tagName);
+  console.log(nombreInput.value);
+
+  // Validar los inputs
+  const nombreValido = checkInputValue(nombreInput, "text") && checkUserName(); // validación de nombre
+  const edadValida = checkInputValue(edadInput, "number");
+
+  // Si ambos inputs son válidos, capturamos los valores
+  if (nombreValido && edadValida) {
+    nombreUsuario = nombreInput.value;
+    edadUsuario = parseInt(edadInput.value);
+
+    // Verificar si el usuario es mayor de edad
+    if (checkUserAge()) {
+      usuarioDialog.close(); // Cerrar el diálogo
+      habilitarBotones(); // Habilitar los botones de compra
+    } else {
+      bloquearBotones(); // Bloquear los botones si es menor de edad
+      usuarioDialog.close();
+    }
+  }
+});
 
 // Función para abrir el dialog para capturar información del usuario
 function abrirDialogoUsuario() {
@@ -105,10 +172,10 @@ function checkInputValue(inputElement, expectedType) {
 // Verificar que el nombre sea mayor a 3 caracteres
 function checkUserName() {
   const nombreInput = document.getElementById("nombre");
+  console.log(nombreInput.classList.add("input-invalid"));
 
   // Verificar que el tipo del input sea "text"
   if (nombreInput.type !== "text") {
-    nombreInput.classList.add("input-invalid");
     nombreInput.nextElementSibling.textContent =
       "El campo de nombre debe ser de tipo texto";
     return false;
@@ -139,6 +206,8 @@ function checkUserName() {
   // Verificar que el nombre tenga más de 3 caracteres
   if (nombreValue.length <= 3) {
     nombreInput.classList.add("input-invalid");
+    // console.log(nombreInput.className);
+    // setInputClass(nombreInput);
     nombreInput.nextElementSibling.textContent =
       "El nombre debe tener más de 3 caracteres";
     return false;
@@ -149,6 +218,16 @@ function checkUserName() {
   nombreInput.nextElementSibling.textContent = ""; // Resetear mensaje de error
   return true;
 }
+
+// function setInputClass(input) {
+//   const inputs = document.querySelectorAll("#usuarioForm > input");
+
+//   for (let i = 0; i < inputs.length; i++) {
+//     if (input[i].classList.contains("input")) {}
+//       input[i].classList.add("input-invalid");
+//     }
+
+// }
 
 // Verificar si el usuario es mayor de edad
 function checkUserAge() {
@@ -164,33 +243,6 @@ function bloquearBotones() {
   });
 }
 
-// Validar y capturar la información del usuario
-usuarioForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Evitar que el formulario se envíe
-
-  const nombreInput = document.getElementById("nombre");
-  const edadInput = document.getElementById("edad");
-
-  // Validar los inputs
-  const nombreValido = checkInputValue(nombreInput, "text") && checkUserName(); // validación de nombre
-  const edadValida = checkInputValue(edadInput, "number");
-
-  // Si ambos inputs son válidos, capturamos los valores
-  if (nombreValido && edadValida) {
-    nombreUsuario = nombreInput.value;
-    edadUsuario = parseInt(edadInput.value);
-
-    // Verificar si el usuario es mayor de edad
-    if (checkUserAge()) {
-      usuarioDialog.close(); // Cerrar el diálogo
-      habilitarBotones(); // Habilitar los botones de compra
-    } else {
-      bloquearBotones(); // Bloquear los botones si es menor de edad
-      usuarioDialog.close();
-    }
-  }
-});
-
 // Función para habilitar los botones de compra si el usuario es mayor de edad
 function habilitarBotones() {
   const botones = eventosList.querySelectorAll("button");
@@ -204,6 +256,8 @@ function habilitarBotones() {
 // Función para manejar la compra de tickets
 function getTickets(eventoKey) {
   const evento = tickets[eventoKey];
+
+  console.log(evento);
 
   if (evento.tickets > 0) {
     evento.tickets--;
