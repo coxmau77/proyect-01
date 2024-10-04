3c76360bcd5c498931516e5bd5fe9944f9d0cf92
Mauricio Cox <coxmau77@gmail.com>
2024-10-04 18:58:31 -0300

    Configuracion de public con Express js

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 //modulo 15 server.js
 // Modulo 16 - express que va a hace que /public sea la app que va a ver el usuario
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
