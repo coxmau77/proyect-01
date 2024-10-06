8a793970e06cc243d78140f763d0c990c929a386
Mauricio Cox <coxmau77@gmail.com>
2024-10-05 22:48:07 -0300

    trabajando en las rutas

--- routes/user.route.js
+++ routes/user.route.js
@@ -64,8 +64,44 @@ const personajesSimpson = [
   },
 ];
 
+function createUserList(cantUsers) {
+  const usuarios = [];
+
+  for (let i = 1; i <= cantUsers; i++) {
+    const usuario = {
+      uid: i,
+      id: generarMongoId(), // ID similar al de MongoDB
+      name: `Usuario${i}`,
+      email: `usuario${i}@ejemplo.com`,
+      created_at: new Date().toISOString(), // Fecha de creación
+      birthdate: `1990-05-${Math.ceil(Math.random() * 28)}`, // Fecha de nacimiento aleatoria
+      favorite_songs: [
+        // Lista de canciones favoritas
+        `Song -> ${Math.ceil(Math.random() * 100)}`,
+        `Song -> ${Math.ceil(Math.random() * 100)}`,
+        `Song -> ${Math.ceil(Math.random() * 100)}`,
+      ],
+    };
+    usuarios.push(usuario);
+  }
+
+  return usuarios;
+}
+
+const cantUsers = 30;
+const users = createUserList(cantUsers);
+
+// Función para generar un ID similar al de MongoDB
+function generarMongoId() {
+  return (
+    Math.random().toString(16).substring(2, 10) +
+    Math.random().toString(16).substring(2, 10)
+  );
+}
+
 route.get("/all", (request, response) => {
-  response.status(200).send("Debe mostrar todos los usuarios");
+  console.info(`Se crearon ${users.length} usuario/s.`);
+  response.status(200).send(users);
 });
 
 route.get("/personajes", (request, response) => {
@@ -74,31 +110,34 @@ route.get("/personajes", (request, response) => {
 
 route.get("/body", (request, response) => {
   //   console.log(request.body);
-  const { nombre, email, direccion, telefonos } = request.body;
-  console.log(
-    `${nombre}, ${email}, ${direccion.calle}, ${direccion.ciudad}, ${direccion.pais} ${telefonos}`
-  );
-  response
-    .status(200)
-    .send(
-      `${nombre}, ${email}, ${direccion.calle}, ${direccion.ciudad}, ${direccion.pais} ${telefonos}`
-    );
+  const { uid, id, name, email, created_at, birthdate, favorite_songs } =
+    request.body;
+  // console.log(
+  //   `Name: ${name}, Email: ${email}, Created at: ${created_at}, Birth date: ${birthdate} | Favorite: ${favorite_songs}`
+  // );
+  const userData = {
+    uid,
+    id,
+    name,
+    email,
+    created_at,
+    birthdate,
+    favorite_songs,
+  };
+  response.status(200).send(userData);
 });
 
 route.get("/:id", (request, response) => {
   // Convertir el id a número
   const id = parseInt(request.params.id);
-  console.log("id --> ", id);
+  console.log("uid --> ", id);
+  const userFound = users.filter((user) => user.uid === id);
 
-  const personajeFiltrado = personajesSimpson.filter(
-    (personaje) => personaje.id === id
-  );
-
-  if (personajeFiltrado.length > 0) {
-    response.status(200).send(personajeFiltrado);
+  if (userFound.length > 0) {
+    response.status(200).send(userFound);
   } else {
     // Manejo de errores
-    response.status(404).send({ message: "Personaje no encontrado." });
+    response.status(404).send({ message: "Usuario no encontrado." });
   }
 });
 
