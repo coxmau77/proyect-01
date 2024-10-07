c12fa9ec1edf38f63fefbdbd2d3b75290c374e57
Mauricio Cox <coxmau77@gmail.com>
2024-10-07 20:23:16 -0300

    Conexion a mongoDB exitosa

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const path = require("node:path");
 
+const mongoose = require("mongoose");
+const urlDb =
+  "mongodb+srv://coxmau77:HdIC1bRzl3EqjVpm@plataforma-disco.biw6p.mongodb.net/?retryWrites=true&w=majority&appName=plataforma-disco";
+
 // Requerimientos de las rutas
 const userRoute = require("./routes/user.route");
 
@@ -17,7 +21,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use("/api/user", userRoute);
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+// Función para conectar a la base de datos y levantar el servidor
+const startServer = async () => {
+  try {
+    // Conectar a la base de datos MongoDB (sin las opciones obsoletas)
+    await mongoose.connect(urlDb);
+    console.log("Conexión exitosa a la base de datos");
+
+    // Levantar el servidor si la conexión fue exitosa
+    app.listen(port, () => {
+      console.log(`Server listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    // Capturar cualquier error durante la conexión
+    console.error("Error al conectar a la base de datos:", error.message);
+  }
+};
+
+// Iniciar la aplicación
+startServer();
